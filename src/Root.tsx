564a7bfa7cd4fef7791b647c87eefdb2076f5e2a
LinCie <aldiofernanda@protonmail.com>
2024-06-14 23:47:50 +0700

    added header

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,22 @@
+import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
+
 function Root() {
-  return <></>;
+  const [Cookies] = useCookies(["username"]);
+
+  return (
+    <main className="w-full">
+      <header className="fixed inset-x-0 top-0 flex items-center justify-between bg-white px-6 py-4">
+        <div className="flex gap-3">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </div>
+        <div>
+          {Cookies.username ? `Halo, ${Cookies.username}` : "Anda belum login"}
+        </div>
+      </header>
+    </main>
+  );
 }
 
 export default Root;
