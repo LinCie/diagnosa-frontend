ffb9f51f17340b348cd74632b0005679892c04a3
LinCie <aldiofernanda@protonmail.com>
2024-06-17 16:34:59 +0700

    added signup

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 import Root from "./Root";
 import Index from "./pages/Index";
+import Daftar from "./pages/Daftar";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route path="/" element={<Index />} />
+      <Route path="/daftar" element={<Daftar />} />
     </Route>,
   ),
 );
