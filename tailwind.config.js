618d890660b4990faea83e7a52b7748d55e2487d
LinCie <aldiofernanda@protonmail.com>
2024-06-17 08:30:48 +0700

    added flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import * as flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
